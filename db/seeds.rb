5328d953c7b41b374b9c5d47a1081aca0e84e889
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-05 16:03:29 -0600

    Update: seed with description..

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-Pet.create(name: 'max', age: '1')
-Pet.create(name: 'Nala', age: '4')
-Pet.create(name: 'Simba', age: '4')
-Pet.create(name: 'Leroy', age: '3')
-Pet.create(name: 'Jenkins', age: '2')
+Pet.create(name: 'Max', age: '1', description: 'Loves to sleep')
+Pet.create(name: 'Nala', age: '4', description: 'Loves to eat')
+Pet.create(name: 'Simba', age: '4', description: 'Loves to play')
+Pet.create(name: 'Leroy', age: '3', description: 'Loves to chill')
+Pet.create(name: 'Jenkins', age: '2', description: 'Loves to party')
