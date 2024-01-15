25679c386ed73bad4d67ceb87538e0e7ba80ad40
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-14 20:19:09 -0600

    Feat: add turbo stream and conditio in the pet controller.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,6 +8,7 @@ class PetsController < ApplicationController
   end
 
   def new
+    @pet = Pet.new
   end
 
   def edit
@@ -16,7 +17,7 @@ class PetsController < ApplicationController
 
   def update
     @pet = Pet.find(params[:id])
-    @pet.update(params.require( :pet).permit( :name, :age, :description))
+    @pet.update(pet_params)
     redirect_to @pet
   end
 
@@ -27,8 +28,21 @@ class PetsController < ApplicationController
   end
 
   def create
-    @pet = Pet.new(params.require( :pet).permit( :name, :age, :description))
-    @pet.save
-    redirect_to @pet
+    @pet = Pet.new(pet_params)
+
+    respond_to do |format|
+      if @pet.save
+        format.html { redirect_to @pet, notice: 'Pet was successfully created.' }
+      else
+        format.turbo_stream
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :description)
   end
 end
